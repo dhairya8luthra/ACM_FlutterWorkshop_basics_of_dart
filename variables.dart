b5800240d6c03de11041fbbb69a42f0cc05aca04
dhairya8luthra <31945980+dhairya8luthra@users.noreply.github.com>
2023-09-07 18:16:15 +0530

    2cnd
    2

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main(){
+/* You must have encountered languages like java where you have to explicitly specify the data type of a variable to be able to use it. But dart works like Python. Due to "type inference" the data types in dart are determined automatically by the values they are initialized with. */
+/*For example: */
+var name = "ACM";      /*Notice the semicolon at the end*/
+/*This automatically makes the data type of name: String*/
+var count = 5;
+/*This automatically makes the data type of count: Integer*/
+var acmMembers = ['Bhaskar', 'Meghraj', 'Dhairya', 'Pranathi', 'Sakshi'];
+/*This automatically makes the data type of acmMembers: List of Strings*/
+
+}
